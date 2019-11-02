5740cd304fa992a4583ba905ca0dacfc520fb895
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-11-02 10:44:36 -0400

    connection js

---
+++ connection.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Alycat17',
+        database: 'burgerDatabase'
+    });
+
+};
+
+connection.connect();
+module.exports = connection;
